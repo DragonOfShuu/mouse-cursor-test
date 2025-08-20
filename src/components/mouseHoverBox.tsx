29df6e9b8a1c1916b8d978f7f647656a236bb78f
Dragon of Shuu <68718280+DragonOfShuu@users.noreply.github.com>
2025-08-20 00:39:32 -0600

    CSS Complete

--- src/components/mouseHoverBox.tsx
+++ src/components/mouseHoverBox.tsx
@@ -1,14 +1,25 @@
 type Props = {
-    property: string
-    displayName: string
-}
+    property: string;
+    displayName: string;
+};
 
 const MouseHoverDiv = (props: Props) => {
-  return (
-    <div className={"mouse-hover-div"} style={{cursor: props.property}}>
-        <p>{props.displayName}</p>
-    </div>
-  )
-}
+    return (
+        <div
+            className={"mouse-hover-outer-div group"}
+            style={{ cursor: props.property }}
+        >
+            <div
+                className={`
+                  mouse-hover-div absolute group-hover:-translate-x-5 group-hover:-translate-y-5 
+                  group-hover:shadow-2xl group-hover:shadow-primary-900 group-hover:border-8 
+                  border-primary-500 transition-all duration-200 box-border
+                `}
+            >
+                <p>{props.displayName}</p>
+            </div>
+        </div>
+    );
+};
 
-export default MouseHoverDiv
+export default MouseHoverDiv;
